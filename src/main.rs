e828fb40dea6ac542730b474a231cf7803ca026a
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-09 15:57:16 +0700

    moved set arg to a variable

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use clap::{command, Arg, ArgMatches};
 
 fn get_matches() -> ArgMatches {
+    let set_arg = Arg::new("set")
+        .short('s')
+        .long("set")
+        .value_name("key=url")
+        .help("Sets a bookmark using a provided key, and url.");
+
     command!()
-        .arg(
-            Arg::new("set")
-                .short('s')
-                .long("set")
-                .value_name("key=url")
-                .help("Sets a bookmark using a provided key, and url."),
-        )
+        .arg(set_arg)
         .get_matches()
 }
 

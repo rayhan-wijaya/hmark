9bba1d071058888da0c43c3ec86cf6ae38fa7087
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-09 23:14:06 +0700

    updated init_dotfolder with new use

--- src/main.rs
+++ src/main.rs
@@ -22,18 +22,18 @@ struct Cli {
     command: Option<Commands>,
 }
 
-fn init_dotfolder() -> std::io::Result<()> {
+fn init_dotfolder() -> io::Result<()> {
     if let Some(home_dir) = dirs::home_dir() {
         let mut dotfolder_path = PathBuf::new();
 
         dotfolder_path.push(home_dir);
         dotfolder_path.push(".hmark");
 
-        return std::fs::create_dir_all(dotfolder_path)
+        return fs::create_dir_all(dotfolder_path)
     }
 
-    Err(std::io::Error::new(
-        std::io::ErrorKind::Other,
+    Err(io::Error::new(
+        io::ErrorKind::Other,
         "Home directory isn't defined",
     ))
 }
